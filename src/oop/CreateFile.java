bf76cac5569f3872464a471e071f47fe13f6b2c4
Md. Mahdi Hossain Hira <87439984+Mahdi-Hira53@users.noreply.github.com>
2021-10-05 02:51:35 +0600

    Second commit

--- src/oop/CreateFile.java
+++ src/oop/CreateFile.java
@@ -12,10 +12,12 @@ public class CreateFile {
         File file1 = new File(path + "/usernameAndPassword.txt");
         File file2 = new File(path + "/studentInfo.txt");
         File file3 = new File(path + "/adminPanel.txt");
+        File file4 = new File(path + "/teacherInfo.txt");
         try {
             file1.createNewFile();
             file2.createNewFile();
             file3.createNewFile();
+            file4.createNewFile();
             System.out.println("Files are created.");
         } catch (Exception e) {
             //System.out.println(e);
