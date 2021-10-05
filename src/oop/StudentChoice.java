425e414963f24a80c82ccbd02620ecfaada22314
Md. Mahdi Hossain Hira <87439984+Mahdi-Hira53@users.noreply.github.com>
2021-10-05 21:37:36 +0600

    5th commit

--- src/oop/StudentChoice.java
+++ src/oop/StudentChoice.java
@@ -13,9 +13,10 @@ public class StudentChoice {
             System.out.println("1. Your Information");
             System.out.println("2. Other Student Information");
             System.out.println("3. Teacher Information");
-            System.out.println("4. Class Routine");
-            System.out.println("5. Bus Schedule");
-            System.out.println("6. Home");
+            System.out.println("4. CR Information");
+            System.out.println("5. Class Routine");
+            System.out.println("6. Bus Schedule");
+            System.out.println("7. Home");
 
             System.out.print("Enter any option you like: ");
             Scanner input = new Scanner(System.in);
@@ -51,14 +52,33 @@ public class StudentChoice {
                         }
                     break;
                 case 4:
+                    System.out.println("************* Please choose an option *************");
+                    System.out.println("1. Search CR Information");
+                    System.out.println("2. Display CR Information");
+
+                    System.out.print("Enter any option you like: ");
+
+                    choice = input.nextInt();
+
+                    switch (choice) {
+                        case 1:
+                            ClassRepresentativeInformation searchClassRepresentativeInformation = new ClassRepresentativeInformation();
+                            searchClassRepresentativeInformation.searchClassRepresentative();
+                            break;
+                        case 2:
+
+                            //break;
+                    }
+                    break;
+                case 5:
                     RoutineBusSchedule routine = new RoutineBusSchedule();
                     routine.routine();
                     break;
-                case 5:
+                case 6:
                     RoutineBusSchedule busSchedule = new RoutineBusSchedule();
                     busSchedule.schedule();
                     break;
-                case 6:
+                case 7:
                     Home home = new Home();
                     home.startOfEverything();
                     break;
