0bdb7124ebebafafc51de172aa5affa82f9b3efe
Md. Mahdi Hossain Hira <87439984+Mahdi-Hira53@users.noreply.github.com>
2021-10-05 21:36:23 +0600

    6th commit

--- src/oop/AdminPanel.java
+++ src/oop/AdminPanel.java
@@ -39,53 +39,118 @@ public class AdminPanel {
                 {
                     // This gives the admins the options what they want to do
                     System.out.println("************* Please choose an option *************");
-                    System.out.println("1. Add New Student Information");
+                    /*System.out.println("1. Add New Student Information");
                     System.out.println("2. Update Student Information");
                     System.out.println("3. Delete Student Information");
                     System.out.println("4. Add New Teacher Information");
                     System.out.println("5. Update Teacher Information");
                     System.out.println("6. Delete Teacher Information");
+                    System.out.println("6. Add Class Representative Information");
                     System.out.println("7. Update Class Routine");
-                    System.out.println("8. Update Bus Schedule");
-                    System.out.println("9. Home");
+                    System.out.println("8. Update Bus Schedule");*/
+                    System.out.println("1. Student Information");
+                    System.out.println("2. Teacher Information");
+                    System.out.println("3. CR Information");
+                    System.out.println("4. Class Routine & Bus Schedule");
+                    System.out.println("5. Home");
 
                     System.out.print("Enter any option you like: ");
                     choice = input.nextInt();
                     switch (choice)
                     {
                         case 1:
-                            AddStudentInformation addStudentInformation = new AddStudentInformation();
-                            addStudentInformation.addInfo();
+                            System.out.println("****** Please choose an option *******");
+                            System.out.println("1. Add New Student");
+                            System.out.println("2. Update Student");
+                            System.out.println("3. Delete Student");
+
+                            System.out.print("Enter any option you like: ");
+                            choice = input.nextInt();
+                            switch (choice){
+                                case 1:
+                                    AddStudentInformation addStudentInformation = new AddStudentInformation();
+                                    addStudentInformation.addInfo();
+                                    break;
+                                case 2:
+                                    UpdateStudentInfo2 updateStudentInfo2 = new UpdateStudentInfo2();
+                                    updateStudentInfo2.updateInfo2();
+                                    break;
+                                case 3:
+                                    DeleteStudentInformation deleteStudentInformation = new DeleteStudentInformation();
+                                    deleteStudentInformation.deleteStudentInformation();
+                                    break;
+                            }
                             break;
                         case 2:
-                            UpdateStudentInfo2 updateStudentInfo2 = new UpdateStudentInfo2();
-                            updateStudentInfo2.updateInfo2();
+                            System.out.println("****** Please choose an option *******");
+                            System.out.println("1. Add New Teacher");
+                            System.out.println("2. Update Teacher");
+                            System.out.println("3. Delete Teacher");
+
+                            System.out.print("Enter any option you like: ");
+                            choice = input.nextInt();
+                            switch (choice)
+                            {
+                                case 1:
+                                    TeacherInformation addTeacherInformation = new TeacherInformation();
+                                    addTeacherInformation.addTeacherInformation();
+                                    break;
+                                case 2:
+                                    TeacherInformation updateTeacherInformation = new TeacherInformation();
+                                    updateTeacherInformation.updateTeacherInformation();
+                                    break;
+                                case 3:
+                                    TeacherInformation deleteTeacherInformation = new TeacherInformation();
+                                    deleteTeacherInformation.deleteTeacherInformation();
+                                    break;
+                            }
                             break;
                         case 3:
-                            DeleteStudentInformation deleteStudentInformation = new DeleteStudentInformation();
-                            deleteStudentInformation.deleteStudentInformation();
+                            System.out.println("****** Please choose an option *******");
+                            System.out.println("1. Add New CR");
+                            System.out.println("2. Update CR");
+                            System.out.println("3. Delete CR");
+
+                            System.out.print("Enter any option you like: ");
+                            choice = input.nextInt();
+
+                            switch (choice)
+                            {
+                                case 1:
+                                    ClassRepresentativeInformation addClassRepresentativeInformation = new ClassRepresentativeInformation();
+                                    addClassRepresentativeInformation.addClassRepresentativeInformation();
+                                    break;
+                                case 2:
+                                    /*ClassRepresentativeInformation addClassRepresentativeInformation = new ClassRepresentativeInformation();
+                                    addClassRepresentativeInformation.addClassRepresentativeInformation();
+                                    break;*/
+                                case 3:
+                                    /*ClassRepresentativeInformation addClassRepresentativeInformation = new ClassRepresentativeInformation();
+                                    addClassRepresentativeInformation.addClassRepresentativeInformation();
+                                    break;*/
+                            }
                             break;
                         case 4:
-                            TeacherInformation addTeacherInformation = new TeacherInformation();
-                            addTeacherInformation.addTeacherInformation();
+                            System.out.println("****** Please choose an option *******");
+                            System.out.println("1. Update Class Routine");
+                            System.out.println("2. Update Bus Schedule");
+
+                            System.out.print("Enter any option you like: ");
+                            choice = input.nextInt();
+
+                            switch (choice)
+                            {
+                                case 1:
+                                    RoutineBusSchedule routine = new RoutineBusSchedule();
+                                    routine.routine();
+                                    break;
+                                case 2:
+                                    RoutineBusSchedule busSchedule = new RoutineBusSchedule();
+                                    busSchedule.schedule();
+                                    break;
+                            }
                             break;
                         case 5:
-                            TeacherInformation updateTeacherInformation = new TeacherInformation();
-                            updateTeacherInformation.updateTeacherInformation();
-                            break;
-                        case 6:
-                            TeacherInformation deleteTeacherInformation = new TeacherInformation();
-                            deleteTeacherInformation.deleteTeacherInformation();
-                            break;
-                        case 7:
-                            RoutineBusSchedule routine = new RoutineBusSchedule();
-                            routine.routine();
-                            break;
-                        case 8:
-                            RoutineBusSchedule busSchedule = new RoutineBusSchedule();
-                            busSchedule.schedule();
-                            break;
-                        case 9:
                             Home home = new Home();
                             home.startOfEverything();
                             break;
